83758eeaa972922d39b38ad954a14a32134da305
Juan Comesaña <jcomesana@gmail.com>
2018-04-10 15:05:43 +0200

    Read the battery status with a function

--- src/main.c
+++ src/main.c
@@ -18,28 +18,56 @@
 #define REG_CHARGE 0x01
 
 
+/**
+ * Basic battery status.
+ */
+typedef struct _batt_status
+{
+    int percentage;
+    bool charging;
+} batt_status;
+
+/**
+ * Leaves in status the current values.
+ * Returns a non-zero value in case of error.
+ */
+int get_batt_status(batt_status *status);
+
+
 int main()
+{
+    batt_status status;
+    if (!get_batt_status(&status)) {
+        printf("%02d%%%s\n", status.percentage, status.charging? "+": "");
+    } else {
+        printf("An error happened, please check the previous output\n");
+        return 1;
+    }
+}
+
+
+int get_batt_status(batt_status *status)
 {
     int file;
     char *file_name = "/dev/i2c-0";
 
     if ((file = open(file_name, O_RDWR)) < 0) {
         perror("Opening the device");
-        exit(1);
+        return 1;
     }
 
     if (ioctl(file, I2C_SLAVE_FORCE, DATA_ADDRESS) < 0) {
         perror("Setting the device address");
         close(file);
-        exit(1);
+        return 1;
     }
 
     u_int8_t reg = REG_PERCENTAGE;
-    int32_t batt_value = i2c_smbus_read_word_data(file, reg);
+    int32_t value = i2c_smbus_read_word_data(file, reg);
+    status->percentage = value;
     reg = REG_CHARGE;
-    int32_t charge_value = i2c_smbus_read_word_data(file, reg);
-    bool charging = charge_value & 0x40;
-    printf("%02d%%%c\n", batt_value, charging? '+': '\0');
+    value = i2c_smbus_read_word_data(file, reg);
+    status->charging = value & 0x40;
     close(file);
-    printf("Done!\n");
+    return 0;
 }
